039fe1c1907d0177c6fda9f828ab3da70b076513
Bradley Reneer <bradley.reneer@gmail.com>
2016-01-29 14:41:50 -0700

    first version

--- src/org/reneer/verse/Book.java
+++ src/org/reneer/verse/Book.java
@@ -7,10 +7,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 //import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author breneer
